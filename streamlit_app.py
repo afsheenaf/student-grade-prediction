ac2c77cb4d2fab9794ff9e782e04001d25ef3975
Afsheen Afifa <2502307H@student.tp.edu.sg>
2026-07-30 21:45:06 +0800

    mapping values

--- streamlit_app.py
+++ streamlit_app.py
@@ -181,41 +181,8 @@ with right:
         ]
     )
 
-    famrel = st.slider(
-        "Family Relationship Quality",
-        1,
-        5,
-        3
-    )
-
-    st.divider()
-
-st.subheader("🏠 Lifestyle & Support")
-
-freetime = st.selectbox(
-    "Free Time After School",
-    [
-        "Very Low",
-        "Low",
-        "Average",
-        "High",
-        "Very High"
-    ]
-)
-
-goout = st.selectbox(
-    "Going Out With Friends",
-    [
-        "Very Low",
-        "Low",
-        "Average",
-        "High",
-        "Very High"
-    ]
-)
-
-health = st.selectbox(
-    "Current Health Status",
+    famrel = st.selectbox(
+    "Family Relationship Quality",
     [
         "Very Poor",
         "Poor",
@@ -223,111 +190,129 @@ health = st.selectbox(
         "Good",
         "Excellent"
     ]
-)
+    )
 
-dalc = st.selectbox(
-    "Workday Alcohol Consumption",
-    [
-        "Very Low",
-        "Low",
-        "Average",
-        "High",
-        "Very High"
-    ]
-)
+    st.divider()
 
-walc = st.selectbox(
-    "Weekend Alcohol Consumption",
-    [
-        "Very Low",
-        "Low",
-        "Average",
-        "High",
-        "Very High"
-    ]
-)
+st.subheader("🏠 Lifestyle & Support")
+lifestyle_left, lifestyle_right = st.columns(2)
 
-reason = st.selectbox(
-    "Main Reason for Choosing This School",
-    [
-        "Course",
-        "Home",
-        "Reputation",
-        "Other"
-    ]
-)
+with lifestyle_left:
 
-schoolsup = st.radio(
-    "Receives School Support",
-    [
-        "Yes",
-        "No"
-    ],
-    horizontal=True
-)
+    freetime = st.selectbox(
+        "Free Time After School",
+        [
+            "Very Low",
+            "Low",
+            "Average",
+            "High",
+            "Very High"
+        ]
+    )
 
-famsup = st.radio(
-    "Receives Family Educational Support",
-    [
-        "Yes",
-        "No"
-    ],
-    horizontal=True
-)
+    goout = st.selectbox(
+        "Going Out With Friends",
+        [
+            "Very Low",
+            "Low",
+            "Average",
+            "High",
+            "Very High"
+        ]
+    )
 
-paid = st.radio(
-    "Attends Extra Paid Classes",
-    [
-        "Yes",
-        "No"
-    ],
-    horizontal=True
-)
+    health = st.selectbox(
+        "Current Health Status",
+        [
+            "Very Poor",
+            "Poor",
+            "Average",
+            "Good",
+            "Excellent"
+        ]
+    )
 
-activities = st.radio(
-    "Participates in Extra Activities",
-    [
-        "Yes",
-        "No"
-    ],
-    horizontal=True
-)
+    dalc = st.selectbox(
+        "Weekday Alcohol Consumption",
+        [
+            "Very Low",
+            "Low",
+            "Average",
+            "High",
+            "Very High"
+        ]
+    )
 
-nursery = st.radio(
-    "Attended Nursery School",
-    [
-        "Yes",
-        "No"
-    ],
-    horizontal=True
-)
+    walc = st.selectbox(
+        "Weekend Alcohol Consumption",
+        [
+            "Very Low",
+            "Low",
+            "Average",
+            "High",
+            "Very High"
+        ]
+    )
 
-higher = st.radio(
-    "Plans to Pursue Higher Education",
-    [
-        "Yes",
-        "No"
-    ],
-    horizontal=True
-)
+with lifestyle_right:
 
-internet = st.radio(
-    "Internet Access at Home",
-    [
-        "Yes",
-        "No"
-    ],
-    horizontal=True
-)
+        reason = st.selectbox(
+            "Main Reason for Choosing This School",
+            [
+                "Course",
+                "Home",
+                "Reputation",
+                "Other"
+            ]
+        )
 
-romantic = st.radio(
-    "Currently in a Romantic Relationship",
-    [
-        "Yes",
-        "No"
-    ],
-    horizontal=True
-)
+        schoolsup = st.radio(
+            "Receives School Support",
+            ["Yes", "No"],
+            horizontal=True
+        )
+
+        famsup = st.radio(
+            "Receives Family Educational Support",
+            ["Yes", "No"],
+            horizontal=True
+        )
+
+        paid = st.radio(
+            "Attends Extra Paid Classes",
+            ["Yes", "No"],
+            horizontal=True
+        )
+
+        activities = st.radio(
+            "Participates in Extra Activities",
+            ["Yes", "No"],
+            horizontal=True
+        )
+
+        nursery = st.radio(
+            "Attended Nursery School",
+            ["Yes", "No"],
+            horizontal=True
+        )
+
+        higher = st.radio(
+            "Plans to Pursue Higher Education",
+            ["Yes", "No"],
+            horizontal=True
+        )
+
+        internet = st.radio(
+            "Internet Access at Home",
+            ["Yes", "No"],
+            horizontal=True
+        )
+
+        romantic = st.radio(
+            "Currently in a Romantic Relationship",
+            ["Yes", "No"],
+            horizontal=True
+        )
 
 st.divider()
 
@@ -335,4 +320,121 @@ predict = st.button(
     "🎓 Predict Grade",
     use_container_width=True
 )
+
+
+
+if predict:
+
+    # -----------------------------
+    # Convert user-friendly inputs
+    # -----------------------------
+
+    # Gender
+    sex = "M" if sex == "Male" else "F"
+
+    # School
+    school = "MS" if school == "Mousinho da Silveira (MS)" else "GP"
+
+    # Address
+    address = "U" if address == "Urban" else "R"
+
+    # Family size
+    famsize = "GT3" if famsize == "More than 3" else "LE3"
+
+    # Parents living together
+    Pstatus = "T" if Pstatus == "Yes" else "A"
+
+    education_map = {
+        "None": 0,
+        "Primary School": 1,
+        "Lower Secondary": 2,
+        "Upper Secondary": 3,
+        "Higher Education": 4
+    }
+
+    Medu = education_map[Medu]
+    Fedu = education_map[Fedu]
+    traveltime_map = {
+        "<15 minutes": 1,
+        "15-30 minutes": 2,
+        "30-60 minutes": 3,
+        ">60 minutes": 4
+    }
+
+    traveltime = traveltime_map[traveltime]
+
+    studytime_map = {
+        "<2 hours": 1,
+        "2-5 hours": 2,
+        "5-10 hours": 3,
+        ">10 hours": 4
+    }
+
+    studytime = studytime_map[studytime]
+
+    rating_map = {
+        "Very Low": 1,
+        "Low": 2,
+        "Average": 3,
+        "High": 4,
+        "Very High": 5
+    }
+
+    health_map = {
+        "Very Poor": 1,
+        "Poor": 2,
+        "Average": 3,
+        "Good": 4,
+        "Excellent": 5
+    }
+
+    freetime = rating_map[freetime]
+    goout = rating_map[goout]
+
+    dalc = rating_map[dalc]
+    walc = rating_map[walc]
+
+    health = health_map[health]
+
+    job_map = {
+        "At Home": "at_home",
+        "Health Care": "health",
+        "Services": "services",
+        "Teacher": "teacher",
+        "Other": "other"
+    }
+
+    Mjob = job_map[Mjob]
+    Fjob = job_map[Fjob]
+
+
+    guardian_map = {
+        "Mother": "mother",
+        "Father": "father",
+        "Other": "other"
+    }
+
+    guardian = guardian_map[guardian]
+
+    reason_map = {
+        "Course": "course",
+        "Home": "home",
+        "Reputation": "reputation",
+        "Other": "other"
+    }
+
+    reason = reason_map[reason]
+
+    schoolsup = "yes" if schoolsup == "Yes" else "no"
+    famsup = "yes" if famsup == "Yes" else "no"
+    paid = "yes" if paid == "Yes" else "no"
+    activities = "yes" if activities == "Yes" else "no"
+    nursery = "yes" if nursery == "Yes" else "no"
+    higher = "yes" if higher == "Yes" else "no"
+    internet = "yes" if internet == "Yes" else "no"
+    romantic = "yes" if romantic == "Yes" else "no"
+
+    Total_Alcohol = dalc + walc
+
+
 
